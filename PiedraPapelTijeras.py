95b18820273993c93ef7869bc1c5e2c26796936c
Zhofri <zhori77@gmail.com>
2023-11-24 23:54:18 -0500

    P,P,T

---
+++ PiedraPapelTijeras.py
@@ -0,0 +1,6 @@
+import random
+print("¡Bienvenido al juego de Piedra, Papel y Tijera!")
+#se importa "random" para generar elcciones aleatorias
+opciones = ("piedra", "papel", "tijera")
+#se define la lista de opciones que va atener nuestro juego
+
